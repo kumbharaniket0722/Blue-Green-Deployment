fdf8c85676aa15946690f8e03d3774ce231136e3
kumbharaniket0722 <kumbhar.aniket722@gmail.com>
2024-12-23 13:42:00 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "devopsshack_node_sg" {
 }
 
 resource "aws_eks_cluster" "devopsshack" {
-  name     = "aniket-cluster"
+  name     = "devopsshack-cluster"
   role_arn = aws_iam_role.devopsshack_cluster_role.arn
 
   vpc_config {
